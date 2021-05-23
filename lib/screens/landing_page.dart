d2a7fb37729cd4c5d1624db805b62262c84104f0
shradzz00 <sssh1902@gmail.com>
2021-05-23 22:57:56 +0530

    Initial Commit

---
+++ lib/screens/landing_page.dart
@@ -0,0 +1,81 @@
+import 'package:shoppingapp/constants.dart';
+import 'package:shoppingapp/screens/home_page.dart';
+import 'package:shoppingapp/screens/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text("Error: ${snapshot.error}"),
+            ),
+          );
+        }
+
+        
+        if (snapshot.connectionState == ConnectionState.done) {
+
+          
+          return StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, streamSnapshot) {
+              
+              if (streamSnapshot.hasError) {
+                return Scaffold(
+                  body: Center(
+                    child: Text("Error: ${streamSnapshot.error}"),
+                  ),
+                );
+              }
+
+              if(streamSnapshot.connectionState == ConnectionState.active) {
+
+                
+                User _user = streamSnapshot.data;
+
+                
+                if(_user == null) {
+                  
+                  return LoginPage();
+                } else {
+                  
+                  return HomePage();
+                }
+              }
+
+               
+              return Scaffold(
+                body: Center(
+                  child: Text(
+                    "Checking Authentication...",
+                    style: Constants.regularHeading,
+                  ),
+                ),
+              );
+            },
+          );
+        }
+
+        
+        return Scaffold(
+          body: Center(
+            child: Text(
+              "Initialization App...",
+              style: Constants.regularHeading,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
